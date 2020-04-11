de2985804bf60d940aea3bbc631d7a63a0b731fe
zyznull <zhangyanzhao00@gmail.com>
2020-04-11 21:19:01 +0800

    remove useless code

--- tests/test_conversations.py
+++
@@ -1,8 +0,0 @@
-from unittest import TestCase
-from chatterbot.conversation import Statement
-
-class StatementTests(TestCase):
-    def test_serializer(self):
-        statement = Statement(text='A test statement.')
-        data = statement.serialize()
-        self.assertEqual(statement.text, data['text'])
