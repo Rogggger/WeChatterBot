4f1751997f0e165e3c9cd0b261f4e380eefcb6c2
Rogggger <wing.yuanwei@gmail.com>
2020-05-05 13:03:50 +0800

    Bug fix

--- app/wechat/wechat_app.py
+++ app/wechat/wechat_app.py
@@ -1,14 +1,15 @@
-from flask import Flask, request, abort, Blueprint
-from time import time
+import time
 import hashlib
 import xml.etree.cElementTree as et
+
+from flask import request, Blueprint
+
 from app.wechat import msg_template as mt
+from app.libs.http import error_jsonify
 
 wx_response = Blueprint('/api', __name__)
 
 WECHAT_TOKEN = 'gintoki'
-HOST = ''
-app = Flask(__name__)
 
 
 def get_reply(type, question):
@@ -26,36 +27,27 @@ def get_reply(type, question):
 def wechat():
     if request.method == 'GET':
         data = request.args
-        signature = data.get('signature')
-        timestamp = data.get('timestamp')
-        nonce = data.get('nonce')
-        echostr = data.get('echostr')
-        req = {k: data.get(k) for k in mt.signature_key}
-        if not all([req['signature'], req['timestamp'], req['nonce']]):
-            abort(400)
+        req = {k: data[k] for k in mt.signature_key if data.get(k) is not None}
+        check_keys = ('signature', 'timestamp', 'nonce')
+        if not all(k in req for k in check_keys):
+            error_jsonify(100001)
 
         lis = [WECHAT_TOKEN, req['timestamp'], req['nonce']]
         lis.sort()
         temp_str = "".join(lis).encode('utf-8')
 
         sign = hashlib.sha1(temp_str).hexdigest()
-        # print(sign)
-        # 验证签名
+
         if req['signature'] != sign:
-            abort(403)
+            error_jsonify(100002)
         else:
             return req['echostr']
 
     if request.method == 'POST':
-        xmldata = request.get_data()
-        xml_rec = et.fromstring(xmldata)
+        xml_rec = et.fromstring(request.get_data())
 
         req = {k: xml_rec.find(k) for k in mt.msg_key}
-        Content = get_reply(req['MsgType'], req['Content'])  # 根据信息类型获得回复
+        content = get_reply(req['MsgType'], req['Content'])  # 根据信息类型获得回复
 
-        return msg_template.reply_template(req['MsgType']) % (req['FromUserName'], req['ToUserName'], int(time()), req['Content'])
-
-
-# if __name__ == '__main__':
-#     # port:80
-#     app.run(host=HOST, port=80, debug=True)
+        return mt.reply_template(req['MsgType']) % (
+            req['FromUserName'], req['ToUserName'], int(time.time()), content)
