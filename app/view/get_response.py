6d41e11005695d0d0254d795d80a046db5708781
G1NTOKI0522 <826523909@qq.com>
2020-05-08 21:09:22 +0800

    add libs/chatbot to get_response

--- app/view/get_response.py
+++ app/view/get_response.py
@@ -5,6 +5,7 @@ from app.chatterbot.trainers import ChatterBotCorpusTrainer
 from app.chatterbot import *
 from app.chatterbot import response_selection
 from app.chatterbot.trainers import ChatterBotCorpusTrainer
+from app.libs.chatbot import chatbot
 
 bp_response = Blueprint('chatterbot', __name__, url_prefix='/chatterbot')
 
