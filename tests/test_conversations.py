cdce6c673afaa9555388c8028a2e731a9d5ef1c5
Rogggger <wing.yuanwei@gmail.com>
2020-05-13 20:16:38 +0800

    Remove useless tests

--- tests/test_conversations.py
+++
@@ -1,8 +0,0 @@
-from unittest import TestCase
-from app.chatterbot_api.chatterbot.conversation import Statement
-
-class StatementTests(TestCase):
-    def test_serializer(self):
-        statement = Statement(text='A test statement.')
-        data = statement.serialize()
-        self.assertEqual(statement.text, data['text'])
