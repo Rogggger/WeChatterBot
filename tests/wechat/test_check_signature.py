bfa3285e60a168da85123485852872365bdf8d4e
G1NTOKI0522 <826523909@qq.com>
2020-05-14 00:18:18 +0800

    add wechat test case

--- tests/wechat/test_check_signature.py
+++ tests/wechat/test_check_signature.py
@@ -1,6 +1,62 @@
+<<<<<<< HEAD
+import requests
+from unittest import TestCase
+import unittest
+
+from app import create_app
+
+
+class CheckSignatureTestCase(TestCase):
+    def setUp(self):
+        self.app = create_app().test_client()
+        self.pre = '/wx/'
+
+    def test_valid_signature(self):
+        r = self.app.get(self.pre,
+                         query_string={
+                             'timestamp': '1409735669',
+                             'nonce': '1320562132',
+                             'signature':
+                             '4dbdbe7c66c7d80b6b2b59e138a58484f915256b',
+                             'echostr': 'connected'
+                         })
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.data, b'connected')
+
+    def test_invalid_signature(self):
+        r = self.app.get(self.pre,
+                         query_string={
+                             'timestamp': '1409735669',
+                             'nonce': '1320562132',
+                             'signature':
+                             '16120ec1b8dbb870f510d87ce6bc2463eae6ca2b',
+                             'echostr': 'connected'
+                         })
+
+        self.assertEqual(r.status_code, 400)
+        ret = r.data.decode('utf-8')
+        self.assertEqual(ret, '{"error": "服务器内部错误", "code": 10000002}')
+
+    def test_lack_keys(self):
+        r = self.app.get(self.pre,
+                         query_string={
+                             'nonce': '1320562132',
+                             'signature':
+                             '4dbdbe7c66c7d80b6b2b59e138a58484f915256b',
+                             'echostr': 'connected'
+                         })
+        self.assertEqual(r.status_code, 400)
+        ret = r.data.decode('utf-8')
+        self.assertEqual(ret, '{"error": "参数不正确", "code": 10000001}')
+
+
+if __name__ == '__main__':
+
+=======
 import unittest
 from unittest import TestCase
 import requests
+from app.view.wechat import *
 
 
 class CheckSignatureTestCase (TestCase):
@@ -10,26 +66,24 @@ class CheckSignatureTestCase (TestCase):
 
     def test_valid_signature(self):
         r = requests.get(url=self.url, params={'timestamp': '1409735669', 'nonce': '1320562132',
-                                               'signature': '4dbdbe7c66c7d80b6b2b59e138a58484f915256b',
-                                               'echostr': 'connected'})
+                                               'signature': '4dbdbe7c66c7d80b6b2b59e138a58484f915256b', 'echostr': 'connected'})
         self.assertEqual(r.status_code, 200)
         self.assertEqual(r.text, 'connected')
 
     def test_invalid_signature(self):
         r = requests.get(url=self.url, params={'timestamp': '1409735669', 'nonce': '1320562132',
-                                               'signature': '16120ec1b8dbb870f510d87ce6bc2463eae6ca2b',
-                                               'echostr': 'connected'})
+                                               'signature': '16120ec1b8dbb870f510d87ce6bc2463eae6ca2b', 'echostr': 'connected'})
 
         self.assertEqual(r.status_code, 400)
         self.assertEqual(r.text, '{"error": "服务器内部错误", "code": 10000002}')
 
     def test_lack_keys(self):
         r = requests.get(url=self.url, params={
-                         'nonce': '1320562132', 'signature': '4dbdbe7c66c7d80b6b2b59e138a58484f915256b',
-                         'echostr': 'connected'})
+                         'nonce': '1320562132', 'signature': '4dbdbe7c66c7d80b6b2b59e138a58484f915256b', 'echostr': 'connected'})
         self.assertEqual(r.status_code, 400)
         self.assertEqual(r.text, '{"error": "参数不正确", "code": 10000001}')
 
 
 if __name__ == '__main__':
+>>>>>>> add wechat test case
     unittest.main()
