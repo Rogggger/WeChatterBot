3ee56550949e840d06f1ca72770fadcb7479bfea
G1NTOKI0522 <826523909@qq.com>
2020-05-14 18:27:22 +0800

    change to local test

--- tests/wechat/test_answer_msg.py
+++ tests/wechat/test_answer_msg.py
@@ -3,10 +3,19 @@ from unittest import TestCase
 import requests
 import xml.etree.cElementTree as et
 
+# import sys
+# sys.path.append("..")
+from app import create_app
+from app.libs.msgcryptor import WXBizMsgCrypt
 
-class AnswerMsgTestCase (TestCase):
+
+class AnswerMsgTestCase(TestCase):
     def setUp(self):
-        self.url = 'https://chatterbot.yuanw.wang/api/wx/'
+        app = create_app()
+        self.app = app
+        app.config['TESTING'] = True
+
+        self.url = 'http://localhost:5000/wx/'
         self.headers = {'Content-Type': 'text/xml'}
 
     def test_answer_text_msg(self):
@@ -97,18 +106,35 @@ class AnswerMsgTestCase (TestCase):
         <Event><![CDATA[unsubscribe]]></Event>
         <MsgId>1234567890123456</MsgId>
         </xml>'''
+        other_event_data = '''<xml>
+        <ToUserName><![CDATA[toUser]]></ToUserName>
+        <FromUserName><![CDATA[fromUser]]></FromUserName>
+        <CreateTime>1348831860</CreateTime>
+        <MsgType><![CDATA[event]]></MsgType>
+        <Event><![CDATA[click]]></Event>
+        <MsgId>1234567890123456</MsgId>
+        </xml>'''
 
-        r = requests.post(url=self.url, data=sub_event_data,
+        r = requests.post(url=self.url,
+                          data=sub_event_data,
                           headers=self.headers)
         xml_rec = et.fromstring(r.text)
         self.assertEqual(r.status_code, 200)
         self.assertEqual(xml_rec.find('Content').text, '你好哇，我是WeChatterBot!')
 
-        r = requests.post(url=self.url, data=unsub_event_data,
+        r = requests.post(url=self.url,
+                          data=unsub_event_data,
                           headers=self.headers)
         xml_rec = et.fromstring(r.text)
         self.assertEqual(r.status_code, 200)
         self.assertEqual(xml_rec.find('Content').text, '再见，有空再来找我玩~')
+
+        r = requests.post(url=self.url,
+                          data=other_event_data,
+                          headers=self.headers)
+        xml_rec = et.fromstring(r.text)
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(xml_rec.find('Content').text, '哈哈哈哈')
 
     def test_voice_msg(self):
         voice_data = '''<xml>
@@ -149,39 +175,83 @@ class AnswerMsgTestCase (TestCase):
             self.assertEqual(r.text, '{"error": "参数不正确", "code": 10000001}')
 
         # 密文模式
-        param = {'signature': 'signature', 'timestamp': 'timestamp',
-                 'nonce': 'nonce', 'encrypt_type': 'aes'}
-        r = requests.post(url=self.url, params=param,
-                          data=data_lack_type, headers=self.headers)
+        param = {
+            'signature': 'signature',
+            'timestamp': 'timestamp',
+            'nonce': 'nonce',
+            'encrypt_type': 'aes'
+        }
+        r = requests.post(url=self.url,
+                          params=param,
+                          data=data_lack_type,
+                          headers=self.headers)
         self.assertEqual(r.status_code, 400)
         self.assertEqual(r.text, '{"error": "参数不正确", "code": 10000001}')
 
-    def test_cryptor(self):
+    def test_crypt_msg(self):
 
-        data = '''<xml>
-                <ToUserName><![CDATA[gh_10f6c3c3ac5a]]></ToUserName>
-                <FromUserName><![CDATA[oyORnuP8q7ou2gfYjqLzSIWZf0rs]]></FromUserName>
-                <CreateTime>1409735668</CreateTime>
-                <MsgType><![CDATA[text]]></MsgType>
-                <Content><![CDATA[abcdteT]]></Content>
-                <MsgId>6054768590064713728</MsgId>
-                <Encrypt><![CDATA[hyzAe4OzmOMbd6TvGdIOO6uBmdJoD0Fk53REIHvxYtJlE2B655HuD0m8KUePWB3+L
-                rPXo87wzQ1QLvbeUgmBM4x6F8PGHQHFVAFmOD2LdJF9FrXpbUAh0B5GIItb52sn896wVsMSHGuPE328HnRG
-                BcrS7C41IzDWyWNlZkyyXwon8T332jisa+h6tEDYsVticbSnyU8dKOIbgU6ux5VTjg3yt+WGzjlpKn6NPhR
-                jpA912xMezR4kw6KWwMrCVKSVCZciVGCgavjIQ6X8tCOp3yZbGpy0VxpAe+77TszTfRd5RJSVO/HTnifJpX
-                gCSUdUue1v6h0EIBYYI1BD1DlD+C0CR8e6OewpusjZ4uBl9FyJvnhvQl+q5rv1ixrcpCumEPo5MJSgM9ehV
-                sNPfUM669WuMyVWQLCzpu9GhglF2PE=]]></Encrypt></xml>'''
+        data = (
+            "<xml><ToUserName><![CDATA[gh_10f6c3c3ac5a]]></ToUserName><FromUserName>"
+            "<![CDATA[oyORnuP8q7ou2gfYjqLzSIWZf0rs]]></FromUserName><CreateTime>1409735668</CreateTime>"
+            "<MsgType><![CDATA[text]]></MsgType><Content><![CDATA[abcdteT]]></Content><MsgId>6054768590064713728</MsgId>"
+            "<Encrypt><![CDATA[hyzAe4OzmOMbd6TvGdIOO6uBmdJoD0Fk53REIHvxYtJlE2B655HuD0m8KUePWB3+LrPXo87wzQ1QLvbeUgm"
+            "BM4x6F8PGHQHFVAFmOD2LdJF9FrXpbUAh0B5GIItb52sn896wVsMSHGuPE328HnRGBcrS7C41IzDWyWNlZkyyXwon8T332jisa+h6"
+            "tEDYsVticbSnyU8dKOIbgU6ux5VTjg3yt+WGzjlpKn6NPhRjpA912xMezR4kw6KWwMrCVKSVCZciVGCgavjIQ6X8tCOp3yZbGpy0V"
+            "xpAe+77TszTfRd5RJSVO/HTnifJpXgCSUdUue1v6h0EIBYYI1BD1DlD+C0CR8e6OewpusjZ4uBl9FyJvnhvQl+q5rv1ixrcpCumEP"
+            "o5MJSgM9ehVsNPfUM669WuMyVWQLCzpu9GhglF2PE=]]></Encrypt></xml>")
 
-        invalid_param = {'signature': '4dbdbe7c66c7d80b6b2b59e138a58484f915256b', 'timestamp': '1409735669',
-                         'msg_signature': '2222222ffba7e9b25a30732f161a50dee',
-                         'nonce': '1320562132', 'encrypt_type': 'aes'}
+        invalid_param = {
+            'signature': '4dbdbe7c66c7d80b6b2b59e138a58484f915256b',
+            'timestamp': '1409735669',
+            'msg_signature': '2222222ffba7e9b25a30732f161a50dee',
+            'nonce': '1320562132',
+            'encrypt_type': 'aes'
+        }
 
-        # 加解密失败
-        r = requests.post(url=self.url, params=invalid_param,
-                          data=data, headers=self.headers)
+        # 解密失败
+        r = requests.post(url=self.url,
+                          params=invalid_param,
+                          data=data,
+                          headers=self.headers)
         self.assertEqual(r.status_code, 400)
-        # self.assertEqual(r.text, '{"error": "消息加密失败", "code": 10000031}')
+        self.assertEqual(r.text, '{"error": "消息加密失败", "code": 10000031}')
+
+    def test_cryptor(self):
+        encodingAESKey = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFG"
+        to_xml = (
+            "<xml><ToUserName><![CDATA[oia2TjjewbmiOUlr6X-1crbLOvLw]]></ToUserName>"
+            "<FromUserName><![CDATA[gh_7f083739789a]]></FromUserName><CreateTime>1407743423</CreateTime>"
+            "<MsgType><![CDATA[video]]></MsgType><Video><MediaId>"
+            "<![CDATA[eYJ1MbwPRJtOvIEabaxHs7TX2D-HV71s79GUxqdUkjm6Gs2Ed1KF3ulAOA9H1xG0]]></MediaId>"
+            "<Title><![CDATA[testCallBackReplyVideo]]></Title>"
+            "<Description><![CDATA[testCallBackReplyVideo]]></Description></Video>"
+            "</xml>")
+        token = "spamtest"
+        nonce = "1320562132"
+        appid = "wx2c2769f8efd9abc2"
+        # 测试加密接口
+        encryp_test = WXBizMsgCrypt(token, encodingAESKey, appid)
+        ret, encrypt_xml = encryp_test.EncryptMsg(to_xml, nonce)
+        self.assertEqual(ret, 0)
+
+        # 测试解密接口
+        timestamp = "1409735669"
+        msg_sign = "5d197aaffba7e9b25a30732f161a50dee96bd5fa"
+
+        from_xml = (
+            "<xml><ToUserName><![CDATA[gh_10f6c3c3ac5a]]></ToUserName><FromUserName>"
+            "<![CDATA[oyORnuP8q7ou2gfYjqLzSIWZf0rs]]></FromUserName><CreateTime>1409735668</CreateTime>"
+            "<MsgType><![CDATA[text]]></MsgType><Content><![CDATA[abcdteT]]></Content><MsgId>6054768590064713728</MsgId>"
+            "<Encrypt><![CDATA[hyzAe4OzmOMbd6TvGdIOO6uBmdJoD0Fk53REIHvxYtJlE2B655HuD0m8KUePWB3+LrPXo87wzQ1QLvbeUgm"
+            "BM4x6F8PGHQHFVAFmOD2LdJF9FrXpbUAh0B5GIItb52sn896wVsMSHGuPE328HnRGBcrS7C41IzDWyWNlZkyyXwon8T332jisa+h6"
+            "tEDYsVticbSnyU8dKOIbgU6ux5VTjg3yt+WGzjlpKn6NPhRjpA912xMezR4kw6KWwMrCVKSVCZciVGCgavjIQ6X8tCOp3yZbGpy0V"
+            "xpAe+77TszTfRd5RJSVO/HTnifJpXgCSUdUue1v6h0EIBYYI1BD1DlD+C0CR8e6OewpusjZ4uBl9FyJvnhvQl+q5rv1ixrcpCumEP"
+            "o5MJSgM9ehVsNPfUM669WuMyVWQLCzpu9GhglF2PE=]]></Encrypt></xml>")
+        decrypt_test = WXBizMsgCrypt(token, encodingAESKey, appid)
+        ret, decryp_xml = decrypt_test.DecryptMsg(from_xml, msg_sign,
+                                                  timestamp, nonce)
+        self.assertEqual(ret, 0)
 
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(verbosity=2)
