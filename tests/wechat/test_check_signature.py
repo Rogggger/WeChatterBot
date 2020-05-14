645948c41fd612574fa35c7dc3c2b2a8ee824616
G1NTOKI0522 <826523909@qq.com>
2020-05-14 18:27:22 +0800

    change to local test

--- tests/wechat/test_check_signature.py
+++ tests/wechat/test_check_signature.py
@@ -1,35 +1,57 @@
-import unittest
-from unittest import TestCase
 import requests
+from unittest import TestCase
+import unittest
+# import sys
+# sys.path.append("..")
+
+from app import create_app
 
 
-class CheckSignatureTestCase (TestCase):
-
+class CheckSignatureTestCase(TestCase):
     def setUp(self):
-        self.url = 'https://chatterbot.yuanw.wang/api/wx/'
+        app = create_app()
+        self.app = app
+        app.config['TESTING'] = True
+
+        self.url = 'http://localhost:5000/wx/'
 
     def test_valid_signature(self):
-        r = requests.get(url=self.url, params={'timestamp': '1409735669', 'nonce': '1320562132',
-                                               'signature': '4dbdbe7c66c7d80b6b2b59e138a58484f915256b',
-                                               'echostr': 'connected'})
+        r = requests.get(url=self.url,
+                         params={
+                             'timestamp': '1409735669',
+                             'nonce': '1320562132',
+                             'signature':
+                             '4dbdbe7c66c7d80b6b2b59e138a58484f915256b',
+                             'echostr': 'connected'
+                         })
         self.assertEqual(r.status_code, 200)
         self.assertEqual(r.text, 'connected')
 
     def test_invalid_signature(self):
-        r = requests.get(url=self.url, params={'timestamp': '1409735669', 'nonce': '1320562132',
-                                               'signature': '16120ec1b8dbb870f510d87ce6bc2463eae6ca2b',
-                                               'echostr': 'connected'})
+        r = requests.get(url=self.url,
+                         params={
+                             'timestamp': '1409735669',
+                             'nonce': '1320562132',
+                             'signature':
+                             '16120ec1b8dbb870f510d87ce6bc2463eae6ca2b',
+                             'echostr': 'connected'
+                         })
 
         self.assertEqual(r.status_code, 400)
         self.assertEqual(r.text, '{"error": "服务器内部错误", "code": 10000002}')
 
     def test_lack_keys(self):
-        r = requests.get(url=self.url, params={
-                         'nonce': '1320562132', 'signature': '4dbdbe7c66c7d80b6b2b59e138a58484f915256b',
-                         'echostr': 'connected'})
+        r = requests.get(url=self.url,
+                         params={
+                             'nonce': '1320562132',
+                             'signature':
+                             '4dbdbe7c66c7d80b6b2b59e138a58484f915256b',
+                             'echostr': 'connected'
+                         })
         self.assertEqual(r.status_code, 400)
         self.assertEqual(r.text, '{"error": "参数不正确", "code": 10000001}')
 
 
 if __name__ == '__main__':
+
     unittest.main()
