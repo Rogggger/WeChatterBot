1e2283d2d6df23f15608db76641c8f963af1b1fa
zyznull <zhangyanzhao00@gmail.com>
2020-05-14 12:01:00 +0800

    添加了logic_adapter相关的单元测试

---
+++ tests/logic/test_best_match.py
@@ -0,0 +1,125 @@
+from app.chatterbot.logic import BestMatch
+from app.chatterbot.conversation import Statement
+from tests.base_case import ChatBotTestCase
+from app.chatterbot.storage import storage_adapter_new
+
+
+class BestMatchTestCase(ChatBotTestCase):
+    """
+    Unit tests for the BestMatch logic adapter.
+    """
+
+    def setUp(self):
+        super().setUp()
+        self.adapter = BestMatch(self.chatbot)
+
+    def test_no_data(self):
+        """
+        If there is no data to return, an exception should be raised.
+        """
+        statement = Statement(text='你想干什么')
+        self.assertRaises(storage_adapter_new.StorageAdapterNew.EmptyDatabaseException, self.adapter.process, statement)
+
+
+    def test_no_choices(self):
+        """
+        The input should be returned as the closest match if there
+        are no other results to return.
+        """
+        self.chatbot.storage.create(text='罗马')
+
+        statement = Statement(text='你的问题是什么')
+        response = self.adapter.process(statement)
+
+        self.assertEqual(response.text, '罗马')
+        self.assertEqual(response.confidence, 0)
+
+    def test_no_known_responses(self):
+        """
+        A match can be selected which has no known responses.
+        In this case a random response will be returned, but the confidence
+        should be zero because it is a random choice.
+        """
+        from unittest.mock import MagicMock
+
+        self.chatbot.storage.update = MagicMock()
+        self.chatbot.storage.count = MagicMock(return_value=1)
+        self.chatbot.storage.get_random = MagicMock(
+            return_value=Statement(text='罗马')
+        )
+
+        match = self.adapter.process(Statement(text='巴拉巴拉'))
+
+        self.assertEqual(match.confidence, 0)
+        self.assertEqual(match.text, '罗马')
+
+    def test_match_with_no_response(self):
+        """
+        A response to the input should be returned if a response is known.
+        """
+        self.chatbot.storage.create(
+            text='去吃披萨',
+            in_response_to='你想干什么'
+        )
+
+        statement = Statement(text='你想干什么')
+        response = self.adapter.process(statement)
+
+        self.assertEqual(response.text, '去吃披萨')
+        self.assertEqual(response.confidence, 0)
+
+    def test_match_with_response(self):
+        """
+        The response to the input should be returned if a response is known.
+        """
+        self.chatbot.storage.create(
+            text='去吃披萨',
+            in_response_to='你想干什么'
+        )
+        self.chatbot.storage.create(
+            text='你想干什么'
+        )
+
+        statement = Statement(text='你想干什么')
+        response = self.adapter.process(statement)
+
+        self.assertEqual(response.text, '去吃披萨')
+        self.assertEqual(response.confidence, 1)
+
+    def test_excluded_words(self):
+        """
+        Test that the logic adapter cannot return a response containing
+        any of the listed words for exclusion.
+        """
+        self.chatbot.storage.create(
+            text='我喜欢音乐'
+        )
+        self.chatbot.storage.create(
+            text='音乐很无聊',
+            in_response_to='我喜欢音乐'
+        )
+        self.chatbot.storage.create(
+            text='音乐很有趣',
+            in_response_to='我喜欢音乐'
+        )
+
+        self.adapter.excluded_words = ['无聊']
+
+        response = self.adapter.process(Statement(text='我喜欢音乐'))
+
+        self.assertEqual(response.confidence, 1)
+        self.assertEqual(response.text, '音乐很有趣')
+
+    def test_low_confidence_options_list(self):
+        """
+        Test the case that a high confidence response is not known.
+        """
+        self.adapter.default_responses = [
+            Statement(text='没有')
+        ]
+
+        statement = Statement(text='那里有土豆么')
+        match = self.adapter.process(statement)
+
+        self.assertEqual(match.confidence, 0)
+        self.assertEqual(match.text, '没有')
---
+++ tests/test_comparator.py
@@ -0,0 +1,124 @@
+"""
+Test ChatterBot's statement comparison algorithms.
+"""
+
+from unittest import TestCase
+from app.chatterbot.conversation import Statement
+from app.chatterbot import comparisons
+from app.chatterbot import languages,tagging
+
+# set language
+LANGUAGE = languages.CHI
+
+class LevenshteinDistanceTestCase(TestCase):
+    def setUp(self):
+        self.compare = comparisons.LevenshteinDistance(language=LANGUAGE)
+
+    def test_false(self):
+        """
+        Falsy values should match by zero.
+        """
+        # Test first statement is empty
+        statement = Statement(text='')
+        other_statement = Statement(text='你好')
+        value = self.compare(statement, other_statement)
+        self.assertEqual(value, 0)
+
+        # Test latter statement is empty
+        statement = Statement(text='你好')
+        other_statement = Statement(text='')
+        value = self.compare(statement, other_statement)
+        self.assertEqual(value, 0)
+
+        # Test that an exception is not raised
+        # if a statement is initialized with an integer value as its text attribute.
+        statement = Statement(text=2)
+        other_statement = Statement(text='你好')
+        value = self.compare(statement, other_statement)
+        self.assertEqual(value, 0)
+
+    def test_true(self):
+        # Test that text capitalization is ignored.
+        statement = Statement(text='你好今天怎么样')
+        other_statement = Statement(text='你好今天怎么样')
+        value = self.compare(statement, other_statement)
+        self.assertEqual(value, 1)
+
+
+class SpacySimilarityTests(TestCase):
+    def setUp(self):
+        self.compare = comparisons.LevenshteinDistance(language=LANGUAGE)
+
+    def test_false(self):
+        """
+        Falsy values should match by zero.
+        """
+        # Test first statement is empty
+        statement = Statement(text='')
+        other_statement = Statement(text='你好')
+        value = self.compare(statement, other_statement)
+        self.assertEqual(value, 0)
+
+        # Test latter statement is empty
+        statement = Statement(text='你好')
+        other_statement = Statement(text='')
+        value = self.compare(statement, other_statement)
+        self.assertEqual(value, 0)
+
+        # Test that an exception is not raised
+        # if a statement is initialized with an integer value as its text attribute.
+        statement = Statement(text=2)
+        other_statement = Statement(text='你好')
+        value = self.compare(statement, other_statement)
+        self.assertEqual(value, 0)
+
+    def test_true(self):
+        # Test sentences with different stopwords.
+        statement = Statement(text='今天天气怎么样')
+        other_statement = Statement(text='今天天气怎么样啊')
+        value = self.compare(statement, other_statement)
+        self.assertAlmostEqual(value, 0.9, places=1)
+
+        # Test that text capitalization is ignored.
+        statement = Statement(text='你好')
+        other_statement = Statement(text='你好')
+        value = self.compare(statement, other_statement)
+        self.assertAlmostEqual(value, 1, places=1)
+
+
+class JaccardSimilarityTestCase(TestCase):
+    def setUp(self):
+        self.compare = comparisons.LevenshteinDistance(language=LANGUAGE)
+        self.tagger = tagging.PosLemmaTagger(language=LANGUAGE)
+    def test_false(self):
+        """
+        Falsy values should match by zero.
+        """
+        # Test first statement is empty
+        statement = Statement(text='',search_text=self.tagger.get_text_index_string(''))
+        other_statement = Statement(text='你好',search_text=self.tagger.get_text_index_string('你好'))
+        value = self.compare(statement, other_statement)
+        self.assertEqual(value, 0)
+
+        # Test latter statement is empty
+        statement = Statement(text='你好',search_text=self.tagger.get_text_index_string('你好'))
+        other_statement = Statement(text='',search_text=self.tagger.get_text_index_string(''))
+        value = self.compare(statement, other_statement)
+        self.assertEqual(value, 0)
+
+        # Test that an exception is not raised
+        # if a statement is initialized with an integer value as its text attribute.
+        statement = Statement(text=2)
+        other_statement = Statement(text='你好')
+        value = self.compare(statement, other_statement)
+        self.assertEqual(value, 0)
+
+    def test_true(self):
+        # Test that text capitalization is ignored.
+        text = '你好'
+        statement = Statement(text=text,search_text=self.tagger.get_text_index_string(text))
+        other_text='你好'
+        other_statement = Statement(text=other_text,search_text=self.tagger.get_text_index_string(other_text))
+        value = self.compare(statement, other_statement)
+        self.assertEqual(value, 1)
+
