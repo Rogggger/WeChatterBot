16f965997ad4e86a642a3bdd7f11e1c47af97058
G1NTOKI0522 <826523909@qq.com>
2020-05-14 11:32:08 +0800

    fix style lint

--- tests/wechat/test_answer_msg.py
+++ tests/wechat/test_answer_msg.py
@@ -158,10 +158,23 @@ class AnswerMsgTestCase (TestCase):
 
     def test_cryptor(self):
 
-        data = '''<xml><ToUserName><![CDATA[gh_10f6c3c3ac5a]]></ToUserName><FromUserName><![CDATA[oyORnuP8q7ou2gfYjqLzSIWZf0rs]]></FromUserName><CreateTime>1409735668</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[abcdteT]]></Content><MsgId>6054768590064713728</MsgId><Encrypt><![CDATA[hyzAe4OzmOMbd6TvGdIOO6uBmdJoD0Fk53REIHvxYtJlE2B655HuD0m8KUePWB3+LrPXo87wzQ1QLvbeUgmBM4x6F8PGHQHFVAFmOD2LdJF9FrXpbUAh0B5GIItb52sn896wVsMSHGuPE328HnRGBcrS7C41IzDWyWNlZkyyXwon8T332jisa+h6tEDYsVticbSnyU8dKOIbgU6ux5VTjg3yt+WGzjlpKn6NPhRjpA912xMezR4kw6KWwMrCVKSVCZciVGCgavjIQ6X8tCOp3yZbGpy0VxpAe+77TszTfRd5RJSVO/HTnifJpXgCSUdUue1v6h0EIBYYI1BD1DlD+C0CR8e6OewpusjZ4uBl9FyJvnhvQl+q5rv1ixrcpCumEPo5MJSgM9ehVsNPfUM669WuMyVWQLCzpu9GhglF2PE=]]></Encrypt></xml>'''
+        data = '''<xml>
+                <ToUserName><![CDATA[gh_10f6c3c3ac5a]]></ToUserName>
+                <FromUserName><![CDATA[oyORnuP8q7ou2gfYjqLzSIWZf0rs]]></FromUserName>
+                <CreateTime>1409735668</CreateTime>
+                <MsgType><![CDATA[text]]></MsgType>
+                <Content><![CDATA[abcdteT]]></Content>
+                <MsgId>6054768590064713728</MsgId>
+                <Encrypt><![CDATA[hyzAe4OzmOMbd6TvGdIOO6uBmdJoD0Fk53REIHvxYtJlE2B655HuD0m8KUePWB3+L
+                rPXo87wzQ1QLvbeUgmBM4x6F8PGHQHFVAFmOD2LdJF9FrXpbUAh0B5GIItb52sn896wVsMSHGuPE328HnRG
+                BcrS7C41IzDWyWNlZkyyXwon8T332jisa+h6tEDYsVticbSnyU8dKOIbgU6ux5VTjg3yt+WGzjlpKn6NPhR
+                jpA912xMezR4kw6KWwMrCVKSVCZciVGCgavjIQ6X8tCOp3yZbGpy0VxpAe+77TszTfRd5RJSVO/HTnifJpX
+                gCSUdUue1v6h0EIBYYI1BD1DlD+C0CR8e6OewpusjZ4uBl9FyJvnhvQl+q5rv1ixrcpCumEPo5MJSgM9ehV
+                sNPfUM669WuMyVWQLCzpu9GhglF2PE=]]></Encrypt></xml>'''
 
         invalid_param = {'signature': '4dbdbe7c66c7d80b6b2b59e138a58484f915256b', 'timestamp': '1409735669',
-                         'msg_signature': '2222222ffba7e9b25a30732f161a50dee', 'nonce': '1320562132', 'encrypt_type': 'aes'}
+                         'msg_signature': '2222222ffba7e9b25a30732f161a50dee',
+                         'nonce': '1320562132', 'encrypt_type': 'aes'}
 
         # 加解密失败
         r = requests.post(url=self.url, params=invalid_param,
