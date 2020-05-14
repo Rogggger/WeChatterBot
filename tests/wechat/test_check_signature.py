6625d2c61692787fdac01a91ca519cc3dd51c7ae
G1NTOKI0522 <826523909@qq.com>
2020-05-14 11:32:08 +0800

    fix style lint

--- tests/wechat/test_check_signature.py
+++ tests/wechat/test_check_signature.py
@@ -1,7 +1,6 @@
 import unittest
 from unittest import TestCase
 import requests
-import xml.etree.cElementTree as et
 
 
 class CheckSignatureTestCase (TestCase):
@@ -11,20 +10,23 @@ class CheckSignatureTestCase (TestCase):
 
     def test_valid_signature(self):
         r = requests.get(url=self.url, params={'timestamp': '1409735669', 'nonce': '1320562132',
-                                               'signature': '4dbdbe7c66c7d80b6b2b59e138a58484f915256b', 'echostr': 'connected'})
+                                               'signature': '4dbdbe7c66c7d80b6b2b59e138a58484f915256b',
+                                               'echostr': 'connected'})
         self.assertEqual(r.status_code, 200)
         self.assertEqual(r.text, 'connected')
 
     def test_invalid_signature(self):
         r = requests.get(url=self.url, params={'timestamp': '1409735669', 'nonce': '1320562132',
-                                               'signature': '16120ec1b8dbb870f510d87ce6bc2463eae6ca2b', 'echostr': 'connected'})
+                                               'signature': '16120ec1b8dbb870f510d87ce6bc2463eae6ca2b',
+                                               'echostr': 'connected'})
 
         self.assertEqual(r.status_code, 400)
         self.assertEqual(r.text, '{"error": "服务器内部错误", "code": 10000002}')
 
     def test_lack_keys(self):
         r = requests.get(url=self.url, params={
-                         'nonce': '1320562132', 'signature': '4dbdbe7c66c7d80b6b2b59e138a58484f915256b', 'echostr': 'connected'})
+                         'nonce': '1320562132', 'signature': '4dbdbe7c66c7d80b6b2b59e138a58484f915256b',
+                         'echostr': 'connected'})
         self.assertEqual(r.status_code, 400)
         self.assertEqual(r.text, '{"error": "参数不正确", "code": 10000001}')
 
