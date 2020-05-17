8443d7dac2286590ba9d78717e039ed21722b291
zyznull <zhangyanzhao00@gmail.com>
2020-05-17 21:42:05 +0800

    修改微信单元测试错误报告顺序不一致的问题，单元测试汇总

--- tests/wechat/test_check_signature.py
+++ tests/wechat/test_check_signature.py
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
-import requests
-from unittest import TestCase
 import unittest
-
+from unittest import TestCase
 from app import create_app
 
 
@@ -35,7 +32,8 @@ class CheckSignatureTestCase(TestCase):
 
         self.assertEqual(r.status_code, 400)
         ret = r.data.decode('utf-8')
-        self.assertEqual(ret, '{"error": "服务器内部错误", "code": 10000002}')
+        self.assertEqual(ret,'{"code": 10000002, "error": "服务器内部错误"}')
+        #self.assertEqual(ret, '{"error": "服务器内部错误", "code": 10000002}')
 
     def test_lack_keys(self):
         r = self.app.get(self.pre,
@@ -47,43 +45,7 @@ class CheckSignatureTestCase(TestCase):
                          })
         self.assertEqual(r.status_code, 400)
         ret = r.data.decode('utf-8')
-        self.assertEqual(ret, '{"error": "参数不正确", "code": 10000001}')
-
-
-if __name__ == '__main__':
-
-=======
-import unittest
-from unittest import TestCase
-import requests
-from app.view.wechat import *
-
-
-class CheckSignatureTestCase (TestCase):
-
-    def setUp(self):
-        self.url = 'https://chatterbot.yuanw.wang/api/wx/'
-
-    def test_valid_signature(self):
-        r = requests.get(url=self.url, params={'timestamp': '1409735669', 'nonce': '1320562132',
-                                               'signature': '4dbdbe7c66c7d80b6b2b59e138a58484f915256b', 'echostr': 'connected'})
-        self.assertEqual(r.status_code, 200)
-        self.assertEqual(r.text, 'connected')
-
-    def test_invalid_signature(self):
-        r = requests.get(url=self.url, params={'timestamp': '1409735669', 'nonce': '1320562132',
-                                               'signature': '16120ec1b8dbb870f510d87ce6bc2463eae6ca2b', 'echostr': 'connected'})
-
-        self.assertEqual(r.status_code, 400)
-        self.assertEqual(r.text, '{"error": "服务器内部错误", "code": 10000002}')
-
-    def test_lack_keys(self):
-        r = requests.get(url=self.url, params={
-                         'nonce': '1320562132', 'signature': '4dbdbe7c66c7d80b6b2b59e138a58484f915256b', 'echostr': 'connected'})
-        self.assertEqual(r.status_code, 400)
-        self.assertEqual(r.text, '{"error": "参数不正确", "code": 10000001}')
-
+        self.assertEqual(ret, '{"code": 10000001, "error": "参数不正确"}')
 
 if __name__ == '__main__':
->>>>>>> add wechat test case
     unittest.main()
